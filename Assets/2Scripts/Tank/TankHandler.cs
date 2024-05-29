dfc8697f1f3b6b8de9ae0830b48f6d1f6e327818
yunmin <84099396+zabd0ng@users.noreply.github.com>
2024-05-29 16:30:15 +0900

    인게임 탱크 방향 랜덤화

--- Assets/2Scripts/Tank/TankHandler.cs
+++ Assets/2Scripts/Tank/TankHandler.cs
@@ -71,6 +71,8 @@ public class  TankHandler : MonoBehaviour
         tankUIHandler = GetComponent<TankUIHandler>(); // 탱크 UI 핸들러 컴포넌트 할당
         rayOffset = new Vector3(0.15f,0,0);
 		powerCoefficient = InGameManager.IT.powerCoefficient;
+        direction = Random.Range(0f, 1f) >= 0.5f ?
+             1 : -1;
     }
 
     public virtual void Start()
@@ -107,13 +109,13 @@ public class  TankHandler : MonoBehaviour
             }
 
             if (Input.GetKey(KeyCode.LeftArrow)) // 카메라 왼쪽으로 이동
-                InGameManager.IT.CamMove(-0.15f, 0);
+                InGameManager.IT.CamMove(-0.2f, 0);
             else if (Input.GetKey(KeyCode.RightArrow)) // Right
-                InGameManager.IT.CamMove(0.15f, 0);
+                InGameManager.IT.CamMove(0.2f, 0);
             else if (Input.GetKey(KeyCode.UpArrow)) // Up
-                InGameManager.IT.CamMove(0, 0.15f);
+                InGameManager.IT.CamMove(0, 0.2f);
             else if (Input.GetKey(KeyCode.DownArrow)) // Up
-                InGameManager.IT.CamMove(0, -0.15f);
+                InGameManager.IT.CamMove(0, -0.2f);
 
             return; // 줌인 중에는 탱크 못움직이게
         } else if (Input.GetKeyUp(KeyCode.Z)) {
