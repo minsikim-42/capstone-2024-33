e6279ba8de55e051086df1c58f9467a12ead01c3
woojin211 <wj21100@naver.com>
2024-06-04 13:15:40 +0900

    MobileInput_3
    MobileInput_3

--- Assets/2Scripts/MobileInputManager.cs
+++ Assets/2Scripts/MobileInputManager.cs
@@ -27,6 +27,33 @@ public class MobileInputManager : MonoBehaviour
 
     void Update()
     {
+        isLeft = false;
+        isRight = false;
+        isUp = false;
+        isDown = false;
 
+        for (int i=0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began: // at first
+                    
+                    break;
+                case TouchPhase.Moved: // moving
+                    if (fingerId == touch.fingerId)
+                    {
+                        
+                    }
+                    break;
+                case TouchPhase.Ended: // del
+                    if (fingerId == touch.fingerId)
+                    {
+                        fingerId = -1;
+                    }
+                    break;
+            }
+        }
     }
 }
